345af28e4088a09c4ec938ee66347c2a5c7c422a
macPro <clairelu3@gmail.com>
2024-03-18 17:17:45 -1000

    changed carousel thirdparty library"

--- app/components/HomeCarousel/index.tsx
+++ app/components/HomeCarousel/index.tsx
@@ -1,14 +1,15 @@
 'use client'
-import React from 'react'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import { Carousel } from 'react-responsive-carousel'
-import HomeSlide from '../HomeSlide'
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Pagination, Navigation } from 'swiper/modules';
+import HomeSlide from '../HomeSlide';
 
 export default function HomeCarousel() {
-  const handleSlideClick = () => {
-    console.log('Slide')
-  }
-
   const slidesData = [
     { text: 'Do a breathing exercise', color: 'bg-red-500' },
     { text: 'Gratitude List', color: 'bg-green-500' },
@@ -19,19 +20,22 @@ export default function HomeCarousel() {
     { text: 'Call a friend', color: 'bg-red-500' },
     { text: 'Take a shower', color: 'bg-green-500' },
     { text: 'Take a nap', color: 'bg-blue-500' },
-  ]
+  ];
 
   return (
-    <Carousel
-      showArrows={true}
-      infiniteLoop={true}
-      onClickItem={handleSlideClick}
-      showThumbs={false}
-      className='w-full mx-auto'
-    >
+    <Swiper
+    pagination={{
+      type: 'fraction',
+    }}
+    navigation={true}
+    modules={[Pagination, Navigation]}
+    className="mySwiper"
+  >
       {slidesData.map((slide, index) => (
-        <HomeSlide key={index} text={slide.text} color={slide.color} />
+        <SwiperSlide key={index}>
+          <HomeSlide text={slide.text} color={slide.color} />
+        </SwiperSlide>
       ))}
-    </Carousel>
-  )
+    </Swiper>
+  );
 }
