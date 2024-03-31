63ec730e1f38c04bef89b24219352eeb61cddfad
macPro <clairelu3@gmail.com>
2024-03-30 22:44:02 -1000

    export website

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -18,15 +18,15 @@ function About() {
         <div className="text-center flex flex-col items-center justify-center w-full pt-24 md:pt-0">
           <h1 className="text-4xl md:text-5xl font-extrabold pb-4">About Us</h1>
           <p className="text-lg md:text-xl font-medium mb-6 max-w-4xl">
-            In a world that moves fast, it's easy to lose sight of our emotional
+            In a world that moves fast, it&apos;s easy to lose sight of our emotional
             health. Our mission is to provide everyone with accessible,
             immediate tools to calm down, lift moods, and offer a guiding light
-            out of moments of darkness. We believe in the power of simple acts —
-            like breathing exercises, mindful meditation, or even a quick nap —
+            out of moments of darkness. We believe in the power of simple acts &mdash;
+            like breathing exercises, mindful meditation, or even a quick nap &mdash;
             to transform your day.
           </p>
           <p className="text-lg md:text-xl font-medium mb-6 max-w-4xl">
-            Whether you're experiencing stress, anxiety, or feeling overwhelmed,
+            Whether you&apos;re experiencing stress, anxiety, or feeling overwhelmed,
             our platform is here to support your journey toward emotional
             balance. From practical tips to engaging activities, we offer a
             variety of resources tailored to foster mental wellness and
@@ -34,7 +34,7 @@ function About() {
           </p>
           <p className="text-lg md:text-xl font-medium mb-6 max-w-4xl">
             Join us in embracing a healthier, more mindful approach to managing
-            emotions and navigating life's challenges. Your journey to emotional
+            emotions and navigating life&apos;s challenges. Your journey to emotional
             well-being starts here.
           </p>
           <p className="text-lg md:text-xl font-medium italic max-w-4xl">
